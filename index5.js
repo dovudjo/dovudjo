692458a8c1313bb2182a6950082ef7575a7cef5e
dovudjo <mirwebdovudjon@gmail.com>
2024-06-18 12:24:33 +0500

    index5.js

---
+++ index5.js
@@ -0,0 +1,13 @@
+    let  pop_1 =prompt("")
+    let arr = [
+        "Olam" , "Banan"  , "limon" 
+    ]
+
+
+    if(pop_1 == -1){
+console.log(arr.pop())
+    }
+    if(pop_1 == -2){
+console.log(arr.shift())
+    }
+    alert(arr)
